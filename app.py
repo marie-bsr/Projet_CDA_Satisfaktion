2ae056a551bc67c17c5dfb472e7a62f3734067e8
Martin Gasque <mgasque.pro@gmail.com>
2019-11-19 15:26:41 +0100

    Liens

--- app.py
+++ app.py
@@ -6,36 +6,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Bonjour, vous êtes situé a la racine "
-
-
-@app.route('/profil')
-def profil(utilisateur_non_identifie):
-    if utilisateur_non_identifie:
-        return(redirect(url_for('page_de_login')))
-    else:
-        return(redirect(url_for('page_accueil')))
-
-# @app.route('/login')
-# def page_de_login():
-#     return render_template("connection.html", connected = 0)
-
+    print( "Bonjour, vous êtes situé a la racine du site, vous allez être redirigé.")
+    return redirect(url_for('page_de_login'))
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
+def page_de_login():
     error = None
     if request.method == "POST":
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'admin@admin.admin' or request.form['password'] != 'admin':
             error = 'Identifiants invalides, veuillez réessayer.'
         else:
             return redirect(url_for('page_accueil'))
 
-    return render_template("connection.html", connected = 0, error = error)
+    return render_template("connection.html", connected = 0, error = error, profil = {})
 
 
 @app.route('/accueil')
 def page_accueil():
-    return render_template("page.html", connected = 1, profils = {"nom" : "Anthony", "role" : "admin"})
+    return render_template("page.html", connected = 1, profil = {"nom" : "Anthony", "role" : "admin"})
+
+
+@app.route('/admin')
+def page_admin():
+    return render_template("admin.html", connected = 1, profil = {"nom" : "Anthony", "role" : "admin"}, admin_type="administrateur")
+
+@app.route('/admin/utilisateurs')
+def admin_utilisateurs():
+    return render_template("admin.html", connected = 1, profil = {"nom" : "Anthony", "role" : "admin"}, admin_type="utilisateurs")
+
+@app.route('/admin/formations')
+def admin_formations():
+    return render_template("admin.html", connected = 1, profil = {"nom" : "Anthony", "role" : "admin"}, admin_type="formations")
+
+@app.route('/admin/etudiants')
+def admin_etudiants():
+    return render_template("admin.html", connected = 1, profil = {"nom" : "Anthony", "role" : "admin"}, admin_type="étudiants")
+
 
 
 if __name__ == '__main__':
