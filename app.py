60d9300073c5349cae60cb38bf9359fcdfe745e5
Gwen-Rullier <laurent.gwenaelle@gmail.com>
2019-11-21 17:07:30 +0100

    first commit Gwen

--- app.py
+++ app.py
@@ -4,6 +4,83 @@
 from flask import Flask, request, redirect, url_for, render_template
 app = Flask(__name__)
 
+import mysql.connector
+from mysql.connector import Error
+
+#connexion à la BDD
+try:
+    connection = mysql.connector.connect(host='da.cefim-formation.org',
+                                         database='MGMO_CDA',
+                                         user='MGMO_CDA',
+                                         password='MGMO')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if (connection.is_connected()):
+        #cursor.close()
+        #connection.close()
+        print("MySQL connection is closed")
+
+#requete pour recupérer toutes les info d'une table
+def getTable(table_nom) :
+    cursor = connection.cursor(dictionary=True)
+    sql = (f"SELECT * FROM {table_nom}")
+    cursor.execute(sql) 
+    values = []
+    for entry in cursor:
+        values.append(entry)
+    return values
+
+def mafonctioncompteur (codeSql) :
+    cursor = connection.cursor()
+    cursor.execute(codeSql) 
+    compteur = []
+    compteur=cursor.fetchone()[0]
+    #for entry in cursor:
+    #    compteur.append(entry)
+    return compteur
+
+
+#page graphique Gwen
+@app.route('/graphique')
+def page_graph () :
+
+    tab_compteur=[0, 0, 0, 0, 0]
+    #récupération des étiquettes
+    cursor = connection.cursor(dictionary=True)
+    sql = ("SELECT DISTINCT rep_progression  FROM questionnaire_quanti")
+    cursor.execute(sql) 
+    values = []
+    for entry in cursor:
+        values.append(entry)
+
+    #récupère la liste des acceptable
+    sql_accep = ("SELECT COUNT(id) FROM questionnaire_quanti WHERE rep_progression='Acceptable' ")
+    tab_compteur[2] = mafonctioncompteur(sql_accep)
+    #récupère la liste des insatisfaisant
+    sql_insatisf = ("SELECT COUNT(id) FROM questionnaire_quanti WHERE rep_progression='Insatisfaisant' ")
+    tab_compteur[1]=mafonctioncompteur(sql_insatisf)
+    #récupère la liste des excellent
+    sql_excell = ("SELECT COUNT(id) FROM questionnaire_quanti WHERE rep_progression='Excellent'")
+    tab_compteur[4]=mafonctioncompteur(sql_excell)
+    #récupère le nombre de insuffisant   
+    sql_insuff= ("SELECT COUNT(id) FROM questionnaire_quanti WHERE rep_progression='Très insuffisant'")
+    tab_compteur[0]=mafonctioncompteur(sql_insuff)
+    #récupère la liste des satisfaisant
+    sql_satisf= ("SELECT COUNT(id) FROM questionnaire_quanti WHERE rep_progression='Satisfaisant'")
+    tab_compteur[3]=mafonctioncompteur(sql_satisf)
+    return render_template(
+        "graphGwen.html", 
+        resultetiquette = values,
+        results=tab_compteur
+    )
+
 @app.route('/')
 def index():
     return "Bonjour, vous êtes situé a la racine "
@@ -40,3 +117,10 @@ def page_accueil():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+#page graphique Gwen camembert
+@app.route('/graphique_camembert')
+def page_graph2 () :
+    return render_template("graphGwen2.html")
--- static/main.js
+++ static/main.js
@@ -15,3 +15,43 @@ function envoiConfirmation(message){
 function envoiMessage(message){
     alert(message)
 }
+
+//fonction graphique en bar
+function graphBar (results){
+                
+                var ctx = document.getElementById('myChart');
+                var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [ 'Très insuffisant', 'Insatisfaisant', 'Acceptable', 'Satisfaisant', 'Excellent'],
+        datasets: [{
+            label: 'SATISFAKTION sur la progression',
+            data: results,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+}
